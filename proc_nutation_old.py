9a77209ab676d98fb2ac8f05557a01d1786d5cf9
Warren Foster Kincaid <wfkincai@syr.edu>
2024-06-27 19:36:06 -0400

    WK nutation on new probe, moved .h5 files to separate folder in WK_exp

--- proc_nutation_old.py
+++ proc_nutation_old.py
@@ -1,21 +1,23 @@
 from pylab import *
 from pyspecdata import *
 from scipy.optimize import minimize
-from pyspecProcScripts import *
+from pyspecProcScripts import hermitian_function_test,zeroth_order_ph,lookup_table,fl_mod,DCCT,select_pathway
 from sympy import symbols
 from numpy import *
 fl = fl_mod()
 t2 = symbols('t2')
 logger = init_logging("info")
-max_kHz = 3.9
-signal_pathway = {'ph1':0,'ph2':1}
-for searchstr,exptype,nodename,postproc,freq_slice in [
-    #['230623_pR_batch230605_N187_a_nutation_1','ODNP_NMR_comp/nutation','nutation',
-    #    'spincore_nutation_v3',(-500,450)]
-    ['240627_200uM_TEMPOL_newprobe_nutation', 'ODNP_NMR_comp/nutation', 'nutation_13',
-        'spincore_nuation_v4', (-500,500)]
+max_kHz = 200
+signal_pathway = {'ph1':1,'ph2':0}
+for searchstr,exp_type,nodename,postproc,freq_slice in [
+    #['201211_Ni_sol_probe_nutation_1','nutation','nutation',
+    #    'spincore_nutation_v1',(-5000,13000)],
+    #['210302_210302_Ni_cap_probe_nutation_1','nutation','nutation',
+    #    'spincore_nutation_v1',(-4e3,1.2e4)]
+    ['240627_200uM_TEMPOL_newprobe_nutation','ODNP_NMR_comp/nutation','nutation_13',
+        'spincore_nutation_v4',(-1900,1900)]
     ]:
-    s = find_file(searchstr,exp_type=exptype,expno=nodename,postproc=postproc,
+    s = find_file(searchstr,exp_type=exp_type,expno=nodename,postproc=postproc,
             lookup=lookup_table,fl=fl)
     s_fullsw = s.C
     s.ift('t2')
@@ -32,13 +34,15 @@ for searchstr,exptype,nodename,postproc,freq_slice in [
         plen = s.get_prop('acq_params')['p90_us']*1e-6
         logger.info(strm('pulse length is:',plen))
     s = s['t2':freq_slice]
-    #s['indirect':-1] *= -1
     s.ift('t2')
     s.set_units('t2','s')
-    best_shift = 0.0035
-    #s.setaxis('t2',lambda x: x-best_shift).register_axis({'t2':0})
+    best_shift,_ = hermitian_function_test(select_pathway(s,signal_pathway),
+            echo_before = s.get_prop('acq_params')['tau']*1e-6*1.5,
+            fl = fl)
+    print(best_shift)
+    s.setaxis('t2',lambda x: x-best_shift).register_axis({'t2':0})
     #{{{zeroth order phasing
-    s /= zeroth_order_ph(select_pathway(s['t2':0].C.mean('nScans'),signal_pathway))
+    s /= zeroth_order_ph(select_pathway(s['t2',0],signal_pathway))
     s.ft('t2')
     fl.next('phase corrected')
     fl.image(s.C.mean('nScans'))
@@ -47,28 +51,30 @@ for searchstr,exptype,nodename,postproc,freq_slice in [
     s = s['t2':(0,None)]
     s *= 2
     s['t2':0] *= 0.5
+    s /= zeroth_order_ph(select_pathway(s['t2':0],signal_pathway))
     s.ft('t2')
     fl.next('phased')
     fl.plot(select_pathway(s.C.mean('nScans'),signal_pathway))
      # {{{ do the centering before anything else!
     # in particular -- if you don't do this before convolution, the
     # convolution doesn't work properly!
-    d = s.C
+    filter_t_const = 10e-3
     d.ift('t2')
     d.set_units('t2','s')
     fl.next('time')
     fl.plot(select_pathway(d.C,signal_pathway),alpha = 0.25)
-    filter_t_const = 10e-3
     apo_fn = exp(-abs((d.fromaxis('t2')-d.get_prop('acq_params')['tau']*1e-6))/filter_t_const)
-    fl.plot(apo_fn*abs(d.C).max(),human_units = False)
+    fl.plot(apo_fn*abs(d.C).max())
     d *= apo_fn
-    d /= zeroth_order_ph(select_pathway(d,signal_pathway))
     d.ft('t2')
     fl.next('apodized')
     fl.plot(select_pathway(d,signal_pathway))
     #}}}
     #{{{ selecting coherence and convolving
-    s = select_pathway(d,signal_pathway)
+    s = select_pathway(s,signal_pathway)
+    fl.next('select $\\Delta p$')
+    s.reorder(['nScans','indirect','t2'])
+    fl.image(s)
     #}}}
     if 'amp' in s.dimlabels:
         s.setaxis('amp',lambda x:x*plen)
@@ -84,23 +90,15 @@ for searchstr,exptype,nodename,postproc,freq_slice in [
         ind_dim = 'p_90'
     else:
         raise ValueError("not sure what the indirect dimenison is!!")
-    for_90 = s.C.mean('nScans')
-    fl.next('p90')
-    for j in range(3):
-        fl.plot(for_90['p_90',j],label = '%d'%j)
-    maxes = []
-    for j in range(len(for_90.getaxis('p_90'))):
-        thismax = abs(for_90['p_90',j]).C.max().real.item()
-        if j ==2:
-            print("last p90 should be inverted")
-            thismax = for_90['p_90',j].C.min().real.item()
-        maxes.append(thismax)
-    fl.next('line for p90')
-    d = nddata(maxes,[-1],['p90'])
-    d.setaxis('p90',for_90.getaxis('p_90'))
-    fl.plot(d, 'o')
-    fit = d.polyfit('p90',order = 1)
-    p90_fine = nddata(np.linspace(6,d.getaxis('p90')[-1],300),'p90')
-    fit_fine = p90_fine.C.eval_poly(fit,'p90')
-    fl.plot(fit_fine,label = 'fit')
+    fl.real_imag('phased data',s)
+    fl.next('FT')
+    title('FT to get $\gamma B_1/a$')
+    s.ft(ind_dim,shift=True)
+    fl.image(s[ind_dim:(-1e3*max_kHz,1e3*max_kHz)])
     fl.show();quit()
+    fl.next('absFT')
+    title('FT to get $\gamma B_1/a$')
+    fl.image(abs(s[ind_dim:(-1e3*max_kHz,1e3*max_kHz)]))
+    gridandtick(gca(),gridcolor=[1,1,1])
+fl.show()
+
