b00eed9e19181a45ab1bdc8073bc21933b64e0ab
Warren Foster Kincaid <wfkincai@syr.edu>
2025-01-30 16:34:16 -0500

    about to start

--- get_ready.sh
+++ get_ready.sh
@@ -2,8 +2,11 @@
 # 
 # Assuming that users have their own directories, and they want to pull up to
 # date scripts into those directories, we can use this
+echo "The following refers to the git for this directory (hopefully the user_exp)"
+echo "==========================================================================="
 git -c core.safecrlf=false commit -a -m "about to start"
 orig_dir="$(pwd)"
+echo "==========================================================================="
 cd /c/apps-su/FLinst
 echo -e "I'm in this branch of FLinst:\n$(git rev-parse --abbrev-ref @)"
 echo -e "is it clean?:\n$(git status -s)"
@@ -13,11 +16,11 @@ cp examples/collect_SC.py $orig_dir
 cp examples/run_generic_echo -e.py $orig_dir
 cp examples/run_nutation.py $orig_dir
 cp examples/run_field_dep_justMW.py $orig_dir
-
+echo "==========================================================================="
 cd /c/git/pyspecdata
 echo -e "I'm in this branch of pyspecdata:\n$(git rev-parse --abbrev-ref @)"
 echo -e "is it clean?:\n$(git status -s)"
-
+echo "==========================================================================="
 cd /c/git/proc_scripts
 echo -e "I'm in this branch of proc_scripts:\n$(git rev-parse --abbrev-ref @)"
 echo -e "is it clean?:\n$(git status -s)"
