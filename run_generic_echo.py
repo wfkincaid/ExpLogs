9d38fed267d0410848a97b541d9d2f005d6062c2
Warren Foster Kincaid <wfkincai@syr.edu>
2025-03-10 15:11:27 -0400

    save files from last time

--- run_generic_echo.py
+++ run_generic_echo.py
@@ -45,9 +45,10 @@ if len(sys.argv) == 2 and sys.argv[1] == "stayput":
     adjust_field = False
 # }}}
 input(
-    "I'm assuming that you've tuned your probe to %f "
+    "I'm assuming that you've tuned your probe to "
+    + "the frequency printed below."
     + "since that's what's in your .ini file."
-    + " Hit enter if this is true"
+    + " Hit enter if this is true: freq = %f"
     % config_dict["carrierFreq_MHz"]
 )
 # {{{ let computer set field
